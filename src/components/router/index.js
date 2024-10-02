1c219de50ab458832ad839227d1165d300ec2221
Mariselvamchellaiya <mariselvamchellaiya@gmail.com>
2024-10-02 19:46:00 +0530

    code clean up and bug fixes

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,13 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Search from "../../views/Search.vue";
-import Ser from "../../views/ser.vue";
 import Create from "../../views/Create.vue";
-import Manage from "../../views/Manage.vue";
 import LandingPage from "../../views/LandingPage.vue";
 import Login from "../../views/LoginPage.vue";
 
 const routes = [
-  // { path: "/", redirect: "/login" }, // Redirect root to login
   {
     path: "/",
     name: "login",
@@ -28,11 +25,6 @@ const routes = [
     name: "create",
     component: Create,
   },
-  {
-    path: "/manage",
-    name: "manage",
-    component: Manage,
-  },
 ];
 
 const router = createRouter({
@@ -41,20 +33,3 @@ const router = createRouter({
 });
 
 export default router;
-
-// import Vue from "vue";
-// import Router from "vue-router";
-// import LandingPage from "@/components/LandingPage.vue"; // Adjust the path as needed
-// import LoginPage from "@/components/LoginPage.vue"; // Adjust the path as needed
-
-// Vue.use(Router);
-
-// export default new Router({
-//   mode: "history",
-//   routes: [
-//     { path: "/", redirect: "/login" }, // Redirect root to login
-//     { path: "/login", name: "login", component: LoginPage },
-//     { path: "/landing", name: "landing", component: LandingPage },
-//     { path: "/search", name: "search", component: SearchComponent }, // Assuming you have a SearchComponent
-//   ],
-// });
